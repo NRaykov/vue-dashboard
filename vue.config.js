4272e5f0b70b48d516378fe646f7a5f908070318
NRaykov <nediotenev@gmail.com>
2021-04-08 16:00:28 +0300

    Deploy configuration

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
-lintOnSave: true,
-outputDir: path.resolve(__dirname, './docs'),
+  lintOnSave: true,
+  outputDir: path.resolve(__dirname, './docs'),
+    publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue-dashboard/'
+  : '/'
 };
