62ef4c6de17fa84fd9df9292572450d17940edd0
NRaykov <nediotenev@gmail.com>
2021-04-08 16:21:19 +0300

    Add publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  lintOnSave: true,
-  outputDir: path.resolve(__dirname, './docs'),
   publicPath: process.env.NODE_ENV === 'production'
   ? '/vue-dashboard/'
-  : '/'
+  : '/',
+  lintOnSave: true,
+  outputDir: path.resolve(__dirname, './docs'),
 };
